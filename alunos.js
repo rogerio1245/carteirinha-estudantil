b1b822697e8ed4695471e69dfec37eee38e0f0cf
rogerio1245 <rogeriocerqueira.al@gmail.com>
2026-01-19 15:47:06 -0300

    Add files via upload

---
+++ alunos.js
@@ -0,0 +1,22 @@
+const alunos = [
+  {
+    hash: "a8476735b37a541a38402a2e7037c79e2d217fe9780e5e34347156ef61eff42b",
+    nome: "Aluno Teste",
+    instituicao: "Estácio",
+    curso: "Ensino Médio",
+    matricula: "0001",
+    cidade: "Anadia",
+    turno: "Noturno",
+    validade: "12/12/2026"
+  },
+{
+    hash: "4914e1b9960e6236dfaacbad1b30c7996074fe980c6d9ea49bb87f65a6fb14ef",
+    nome: "Estelionatária Souza",
+    instituicao: "Ifal",
+    curso: "Letras",
+    matricula: "0002",
+    cidade: "Anadia",
+    turno: "Matutino",
+    validade: "12/12/2026"
+  }
+];
